22eb0496ab2854e2bafda13c0d7682859d383fd0
Ahkal <bp@sojourn.llc>
2022-09-22 17:01:32 -0400

    removes unused

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-let boxes = document.getElementsByClassName("box");
+let arrays = Array.from(document.getElementsByClassName("box"));
+let container = document.querySelector(".boxes");
+let rightBtn = document.getElementById("right");
+let leftBtn = document.getElementById("left");
 
-// convert html colllect ^ to an array
-console.log(Array.isArray(boxes)); // F
-console.log(Array.isArray(Array.from(boxes))); // T
-Array.from(boxes).forEach((box) => {
-  console.log(box);
+leftBtn.addEventListener("click", () => {
+  let rotate = document.getElementById("rotateBy");
+  rotateArray("L", arrays, (rotate = 1));
+});
+rightBtn.addEventListener("click", () => {
+  let rotate = document.getElementById("rotateBy");
+  rotateArray("R", arrays, (rotate = 1));
 });
 
-let divs = document.getElementsByTagName("div");
-console.log(divs);
-console.log(boxes[1]);
+function reverseArr(arr, start, end) {
+  console.log(start);
+  console.log(end);
+  while (start < end) {
+    console.log(arr[start]);
+    console.log(arr[end]);
+
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+}
+
+function rotateArray(dir, arr, r) {
+  const length = arr.length - 1;
+  console.log("reversing");
+  arr.reverse();
+  console.log("reversed", arr);
+  console.log("sending arr to function");
+  reverseArr(arr, 0, r - 1);
+  console.log("first reverse complete..");
+  // reverseArr(arr, r, length);
+}
